cf3aa1f5602716c738b462adf338bfc5485866d0
Sebastian Simon <Bastisimon95@googlemail.com>
2025-04-23 11:23:14 +0000

    Get top 100 repos from GitHub

--- slurm/start_slurm_experiment.sh
+++ slurm/start_slurm_experiment.sh
@@ -3,7 +3,7 @@
 PARTITION='edison5a'
 JOB_NAME='ssimon-config-space'
 
-COMMANDS_FILE='./microservice_projects.txt'
+COMMANDS_FILE='./popularity_projects.txt'
 
 NTASKS=$(cat $COMMANDS_FILE | wc -l)
 echo "$(date) Submitting job $JOB_NAME to $PARTITION monitoring $NTASKS configurations."
