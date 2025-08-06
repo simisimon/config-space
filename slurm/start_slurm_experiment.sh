1c0372e5b36d9d969ccaacb3580080d728868d42
Sebastian Simon <ssimon@aspra13.informatik.uni-leipzig.de>
2025-08-06 12:49:58 +0200

    Update slurm data files

--- slurm/start_slurm_experiment.sh
+++ slurm/start_slurm_experiment.sh
@@ -3,10 +3,10 @@
 PARTITION='edison5a'
 JOB_NAME='ssimon-config-space'
 
-COMMANDS_FILE='./projects_final_updated.txt'
+COMMANDS_FILE='./projects_final_edison5a.txt'
 
 NTASKS=$(cat $COMMANDS_FILE | wc -l)
-echo "$(date) Submitting job $JOB_NAME to $PARTITION monitoring $NTASKS configurations."
+echo "$(date) Submitting job $JOB_NAME to $PARTITION monitoring $NTASKS tasks."
 mkdir -p /home/ssimon/logs
 
 sbatch --error="/home/ssimon/logs/%x_%A_%a.err" --output="/home/ssimon/logs/%x_%A_%a.out" --exclusive --array=1-$NTASKS --partition=$PARTITION --job-name=$JOB_NAME ./analysis_job.sh $COMMANDS_FILE
--- slurm/update_projects.py
+++ slurm/update_projects.py
@@ -1,4 +1,6 @@
 import os
+import json
+
 
 def get_project_names_from_folders():
     projects_dir = 'projects'
@@ -33,40 +35,63 @@ def get_failed_project_names_from_folders():
 
 def update_projects():
     project_names = get_project_names_from_folders()
-    print(project_names)
- 
+    #print(project_names)
+
+    new_lines = []
+
+    print("Length of project names:", len(project_names))
+
     with open('projects_final.txt', 'r') as f:
         lines = f.readlines()
-    
-    print(len(lines))
+
+    print("Length of lines:", len(lines))
 
     for line in lines:
-        if any(f"--name={project_name}" in line for project_name in project_names):
-            lines.remove(line)
+        if not any(f"--name={project_name}" in line for project_name in project_names):
+            #lines.remove(line)
+            new_lines.append(line)
 
-    print(len(lines))
-
-    lines.reverse()
+    print(len(new_lines))
 
     with open('projects_final_updated.txt', 'w') as f:
-        f.writelines(lines)
+        f.writelines(new_lines)
 
 
 def failed_projects():
     failed_project_names = get_failed_project_names_from_folders()
 
-    print(failed_project_names)
+    #print(failed_project_names)
 
     with open('projects_final_updated.txt', 'r') as f:
         lines = f.readlines()
 
     failed = []
+    failed_json = []
     for line in lines:
         if any(f"--name={project_name}" in line for project_name in failed_project_names):
             failed.append(line)
+            line_parts = line.split(" ")
+            project_name = ""
+            project_url = ""
+            for part in line_parts:
+                if "--name" in part:
+                    project_name = part.split("=")[-1].strip()
+                if "--url" in part:
+                    project_url = part.split("=")[-1].strip()
+
+            if project_name and project_url:
+                failed_json.append({
+                    "name": project_name,
+                    "url": project_url
+                })
+
+    print("Failed projects count:", len(failed_json))
 
     with open('projects_final_failed.txt', 'w') as f:
         f.writelines(failed)
+
+    with open('projects_final_failed_json.json', 'w') as f:
+        json.dump(failed_json, f, indent=4)
 
 
 if __name__ == "__main__":
